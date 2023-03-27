fb2ad8f127f41bd7d666f62ecfcab2742a407199
Stoma Fedor <fedorstoma2@gmail.com>
2023-03-27 21:14:46 +0300

    add CMake project

---
+++ Homework1CMake/Homework1CMake/Homework1CMake.cpp
@@ -0,0 +1,39 @@
+﻿// Homework1CMake.cpp: определяет точку входа для приложения.
+//
+
+#include <iostream>
+#include <Windows.h>
+
+#include "StatisticsWorker.h"
+#include "FileReader.h"
+
+void Output(vector<string> groups)
+{
+    for (int i = 0; i < groups.size(); ++i)
+        cout << groups[i] << endl;
+}
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+        if (argc != 5)
+            throw invalid_argument("Incorrect command line data");
+        if (to_string(atoi(argv[3])) != argv[3] || to_string(atoi(argv[4])) != argv[4])
+            throw invalid_argument("Incorrectly entered years");
+        int beginYear = atoi(argv[3]), endYear = atoi(argv[4]);
+        SetConsoleCP(1251);
+        SetConsoleOutputCP(1251);
+        FileReader reader;
+        vector<vector<string>> artists = reader.ReadFile(argv[1], 2, 4);
+        vector<vector<string>> types = reader.ReadFile(argv[2], 0, 1);
+        StatisticsWorker worker;
+        int typeId = worker.GetArtistTypeId(types, "Group");
+        vector<string> groups = worker.GetArtistListInCurrentYears(artists, typeId, beginYear, endYear);
+        Output(groups);
+    }
+    catch (exception& ex)
+    {
+        cout << ex.what() << endl;
+    }
+}
---
+++ Homework1CMake/Homework1CMake/src/FileReader.cpp
@@ -0,0 +1,45 @@
+#include "FileReader.h"
+
+vector<vector<string>> FileReader::ReadFile(string filename, int firstCol, int secondCol)
+{
+    vector<vector<string>> arrData;
+    string line;
+    int i = 0;
+    ifstream in(filename);
+    if (in.is_open())
+    {
+        while (getline(in, line))
+        {
+            vector<string> tmp = Split(line, '	', firstCol, secondCol);
+            if (tmp[1] != "\\N")
+                arrData.push_back(tmp);
+        }
+    }
+    else
+        throw invalid_argument("The file path is specified incorrectly");
+    in.close();
+    return arrData;
+}
+
+vector<string> FileReader::Split(string& str, char separator, int firstCol, int secondCol)
+{
+    vector<string> result;
+    string s;
+    int separatorCounter = 0;
+    for (int i = 0; i < str.length(); ++i)
+    {
+        if (str[i] != separator) {
+            s += str[i];
+        }
+        else if (firstCol == separatorCounter || secondCol == separatorCounter) {
+            result.push_back(s);
+            s.clear();
+            separatorCounter++;
+        }
+        else {
+            s.clear();
+            separatorCounter++;
+        }
+    }
+    return result;
+}
